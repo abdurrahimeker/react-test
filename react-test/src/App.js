5f86ba12a67cf5b58af5fcb11a48bd080fffbb59
abdurrahim eker <90318672+abdurrahimeker@users.noreply.github.com>
2022-06-28 17:15:54 +0300

    deneme

---
+++ react-test/src/App.js
@@ -0,0 +1,26 @@
+
+
+  // setTimeout(() => {
+  //   console.log("deneme");
+  // }, 2000)
+
+  // setInterval(() => {
+  //   console.log("deneme");
+  // }, 1111)
+
+  // const sayı = (cb) => {
+  //   cb();
+  // };
+  // sayı(() => {
+  //   console.log("eker")
+  // })
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
