eebfafbb09570644ecab01acd5cd7bc3830b35f5
abdurrahim eker <90318672+abdurrahimeker@users.noreply.github.com>
2022-06-29 11:16:38 +0300

    Update App.js

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -35,22 +35,37 @@
   ]
 
 
-  // push işlemi
+  // push işlemi arraye yeni eleman
   // users.push('ekr')
   // users.push('ekrdfsfds')
 
-  //map işlemi
+  //map işlemi array içinde gezinme
   // users.map((x) => {
   //   console.log(x.surname)
   // })
 
-  // find işlemi
-  // const result = users.find((x) => x.id == 1 || x.id + 1)
+  // find işlemi verdiğimiz koşula uygun arrayi getirir
+  // const result = users.find((x) => x.id == 1)
   // console.log(result)
 
-  
+  //filter işlemi verdiğimiz koşula uygun olani filtreler
+  // const filtered = users.filter((x) => x.name == "mahmut");
+  // console.log(filtered)
 
-function App() {
+  // some işlemi verdiğimiz komut doğruysa true değilse false
+  // const some = users.some((x) => x.surname == "yıldız");
+  // console.log(some)
+
+  // every işlemi verdiğimiz komut için bütün arrayler doğruysa true değilse false
+  // const every = users.every((x) => x.name == "ahmet")
+  // console.log(every);
+
+  // includes komutu arrayın içinde varsa true yoksa false
+  // const meyveler = ['muz','armut','elma']
+  // const includes = meyveler.includes("elma")
+  // console.log(includes);
+
+  function App() {
   return (
     <div className="App">
       
